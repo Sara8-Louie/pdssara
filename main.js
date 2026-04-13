e5ab6aeb5b31eedb3314425b8865a04f2a0296e9
Sara <sara.salviano@escolar.ifrn.edu.br>
2026-04-13 11:12:13 +0000

    Aula 13/04

---
+++ main.js
@@ -0,0 +1,24 @@
+/* console.log("Olá, mundo!!");
+console.log('Olá, mundo!!');
+console.log(`Olá, mundo!!`);
+
+console.log(`Olá, meu nome é Sara Salviano, mas me chamam de "Sara"!`);
+
+const nome = "Sara"
+
+console.log("Olá, ${nome}");
+console.log('Olá, ${nome}');
+console.log(`Olá, ${nome}`);
+
+console.log(15, "Olá,pessoal!", 50.44, undefined, null);
+// O undefined e null não estão funcionando ^ :(
+
+/* A turma do 4 ano é a melhor do IFRN
+Estão aprendendo Programação 
+
+// PARTE MAIS IMPORTANTE DA AULA: :o */
+
+console.log("Eu vou aparecer no HTML LALALALA !!!");
+
+
+
